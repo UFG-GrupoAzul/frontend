6664380ce990c9eed96c1e057120659daddf9803
alexbeserra <alexbeserrab@gmail.com>
2024-10-05 18:08:20 -0300

    Fazendo atividade 2 exec 5

---
+++ Avaliacao2/atividade05/js/par-impar.js
@@ -0,0 +1,27 @@
+function verificarNumero() {
+    const numero = document.getElementById("numero").value;
+    const resultado = document.getElementById("resultado");
+
+    if (numero === "") {
+        resultado.textContent = "Por favor, insira um número.";
+        resultado.style.color = "red";
+        return;
+    }
+
+    const numeroInt = parseInt(numero);
+
+    if (isNaN(numeroInt)) {
+        resultado.textContent = "Entrada inválida. Por favor, insira um número válido.";
+        resultado.style.color = "red";
+        return;
+    }
+
+    if (numeroInt % 2 === 0) {
+        resultado.textContent = `O número ${numeroInt} é Par.`;
+        resultado.style.color = "green";
+    } else {
+        resultado.textContent = `O número ${numeroInt} é Ímpar.`;
+        resultado.style.color = "blue";
+    }
+    document.getElementById("numero").value = null;
+}
